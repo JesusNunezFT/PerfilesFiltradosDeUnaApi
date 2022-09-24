cb3b812a7540257110da7bb1ecef2f212ac1748d
Jesus Nuñez <jesuscm9@gmail.com>
2022-09-24 14:18:36 -0500

    segundo commit todo funcionando

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -15,32 +15,82 @@ export default function Principal() {
   let [profiles, setProfiles] = useState([]);
   let [gender, setGender] = useState([]);
   let [age, setAge] = useState([]);
-
-  // async function handleConsulta(){
-  //   await fetch("https://randomuser.me/api/?results=80")
-  //   .then(response => response.json())
-  //   .then(data => setProfiles(data.results))
-  //   let generos = profiles.map(p => p.gender)
-  //   console.log(generos)
-  //   setGender(generos)
-  // }
+  let [nat, setNat] = useState([]);
+  let [fn, setFn] = useState([]);
+  let [fr, setFr] = useState([]);
 
   async function handleConsulta() {
     let perfiles = await fetch("https://randomuser.me/api/?results=80")
       .then((response) => response.json())
+      
       .then((data) => data.results);
     setProfiles(perfiles);
+
     handleGender(perfiles);
+    console.log(gender);
+
+    handleAge(perfiles);
+    console.log(age);
+
+    handleNat(perfiles);
+    console.log(nat);
+
+    handleFn(perfiles);
+    console.log(fn);
+
+    handleFr(perfiles);
+    console.log(fr);
   }
 
   function handleGender(perfiles) {
+    console.log(perfiles);
     let generos_duplicados = perfiles.map((p) => p.gender);
     let generos = generos_duplicados.filter((item, index) => {
       return generos_duplicados.indexOf(item) === index;
     });
     setGender(generos);
+    console.log(generos);
   }
-  console.log(gender);
+
+  function handleAge(perfiles) {
+    let edades_duplicados = perfiles.map((p) => p.dob.age);
+    let edades = edades_duplicados.filter((item, index) => {
+      return edades_duplicados.indexOf(item) === index;
+    });
+    edades = edades.sort();
+    setAge(edades);
+    console.log(edades);
+  }
+
+  function handleNat(perfiles) {
+    let nacionalidad_duplicados = perfiles.map((p) => p.nat);
+    let nacionalidad = nacionalidad_duplicados.filter((item, index) => {
+      return nacionalidad_duplicados.indexOf(item) === index;
+    });
+    nacionalidad = nacionalidad.sort();
+    setNat(nacionalidad);
+    console.log(nacionalidad);
+  }
+
+  function handleFn(perfiles) {
+    let nacimiento_duplicados = perfiles.map((p) => p.dob.date);
+    let nacimiento = nacimiento_duplicados.filter((item, index) => {
+      return nacimiento_duplicados.indexOf(item) === index;
+    });
+    nacimiento = nacimiento.sort();
+    setFn(nacimiento);
+    console.log(nacimiento);
+  }
+
+  function handleFr(perfiles) {
+    let registro_duplicados = perfiles.map((p) => p.registered.date);
+    let registro = registro_duplicados.filter((item, index) => {
+      return registro_duplicados.indexOf(item) === index;
+    });
+    registro = registro.sort();
+    setFr(registro);
+    console.log(registro);
+  }
 
   return (
     <MDBContainer breakpoint="sm" style={{ height: 800 }}>
@@ -48,7 +98,7 @@ export default function Principal() {
         <MDBCol size="md" className="text-center">
           <MDBTable className="text-center">
             <MDBTableHead className="bg-info shadow-1-strong ">
-              <tr className=".justify-content-center">
+              <tr >
                 <th></th>
                 <th scope="col">
                   <MDBBtn onClick={handleConsulta}>Consultar</MDBBtn>
@@ -59,38 +109,44 @@ export default function Principal() {
 
             <MDBTableBody>
               <tr className="table-primary">
-                <th scope="row">
-                  {" "}
-                  <select className="browser-default custom-select">
-                    <option>Nombre</option>
-                    <option value="1"></option>
-                    <option value="2"></option>
-                    <option value="3"></option>
-                  </select>
+                <th scope="row" className="d-flex justify-content-center">
+                  <th>
+                    <div className="form-outline">
+                      <input type="text" id="form12" className="form-control border border-primary bg-light" />
+                      <label className="form-label" for="form12">
+                        Nombre <MDBIcon fas icon="search" />
+                      </label>
+                      
+                    </div>
+                  </th>
                 </th>
+
                 <td>
+                  <label htmlFor="">Edad</label>
                   <select className="browser-default custom-select">
-                    <option>Edad</option>
-                    <option value="1">15-20</option>
-                    <option value="2">20-30</option>
-                    <option value="3">30-40</option>
-                    <option value="3">40-50</option>
-                    <option value="3">50-60</option>
-                    <option value="3">60-70</option>
+                    <option value="">todas</option>
+                    {age.length &&
+                      age.map((g, index) => {
+                        return <option value={g}>{g}</option>;
+                      })}
                   </select>
                 </td>
                 <td>
+                  <label htmlFor="">F/Nacimiento</label>
                   <select className="browser-default custom-select">
-                    <option>Nacionalidad</option>
-                    <option value="1"></option>
-                    <option value="2"></option>
-                    <option value="3"></option>
+                    <option value="">todos</option>
+                    {fn.length &&
+                      fn.map((g) => {
+                        return <option value={g}>{g}</option>;
+                      })}
                   </select>
                 </td>
               </tr>
               <tr className="table-primary">
                 <th scope="row">
-                  <select className="browser-default custom-select">
+                  <label htmlFor="">Genero</label>
+                  <select onChange={ (e) => setSelectedGender(e.target.value) } className="browser-default custom-select">
+                  
                     <option value="">Todos</option>
                     {gender.length &&
                       gender.map((g) => {
@@ -99,19 +155,23 @@ export default function Principal() {
                   </select>
                 </th>
                 <td>
+                  <label htmlFor="">Nacionalidad</label>
                   <select className="browser-default custom-select">
-                    <option>Fecha de Nacimiento</option>
-                    <option value="1"></option>
-                    <option value="2"></option>
-                    <option value="3"></option>
+                    <option value="">todos</option>
+                    {nat.length &&
+                      nat.map((g, index) => {
+                        return <option value={g}>{g}</option>;
+                      })}
                   </select>
                 </td>
                 <td>
+                  <label htmlFor="">F/Registro</label>
                   <select className="browser-default custom-select">
-                    <option>Fecha De Regristro</option>
-                    <option value="1"></option>
-                    <option value="2"></option>
-                    <option value="3"></option>
+                    <option value="">todos</option>
+                    {fr.length &&
+                      fr.map((g) => {
+                        return <option value={g}>{g}</option>;
+                      })}
                   </select>
                 </td>
               </tr>
@@ -145,7 +205,7 @@ export default function Principal() {
                 <td>{profile.name.first}</td>
                 <td>{profile.dob.age}</td>
                 <td>{profile.gender}</td>
-                <td>{profile.location.country}</td>
+                <td>{profile.nat}</td>
                 <td>{profile.dob.date}</td>
                 <td>{profile.registered.date}</td>
               </tr>
